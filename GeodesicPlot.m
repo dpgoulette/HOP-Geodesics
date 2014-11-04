8be7ebcafc1b9866c0f741131e7f5547fa146ebf
David Goulette <dpgoulette@gmail.com>
2014-11-04 09:53:36 -0800

    Fixed GeodesicPlot.m
    Now it runs!

--- GeodesicPlot.m
+++ GeodesicPlot.m
@@ -1,9 +1,10 @@
 
 fprintf('This script plots the results of HOP and max geodesics.\n')
-fprintf('Choose one of the following options: ')
+fprintf('Would you like to see the geodesics only? Or do you want \n')
+fprintf('to see the step functions for each max as well?\n')
 while true
-   fprintf('  1) Plot the raw data, geodesics AND the geodesic step functions.\n');
-   fprintf('  2) Plot ONLY the raw data and geodesics.\n')
+   fprintf('  1) Geodesics only.\n')
+   fprintf('  2) Geodesics with step functions.\n')
    plot_option = input('Which of the above options do you prefer: ');
    if plot_option==1 || plot_option==2 
       break
@@ -11,11 +12,13 @@ while true
       fprintf('\nERROR: You must enter 1 or 2.\n')
    end
 end
-
+fprintf('\n')
+fprintf('\n')
+pause(.2)
 if plot_option == 1
    clear plot_option
-   GeosWithStepFunc(DT,GoodIndex,maxconnect,maxclass,maxindex,GoodMaxGeodesics)
+   GeosOnly(DT,GoodIndex,maxconnect,maxclass,maxindex,GoodMaxGeodesics)
 else
    clear plot_option
-   GeosOnly(DT,GoodIndex,maxconnect,maxclass,maxindex,GoodMaxGeodesics)
+   GeosWithStepFunc(DT,GoodIndex,maxconnect,maxclass,maxindex,GoodMaxGeodesics)
 end
