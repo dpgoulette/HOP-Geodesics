40559353891d7a6a62db598e296f61799a6d85db
David Goulette <dpgoulette@gmail.com>
2014-11-04 13:27:00 -0800

    HOPGeodesics_main.m  Added a plot option.
    Now you can just run the main and you don't have to run the plot
    function separately.

--- HOPGeodesics_main.m
+++ HOPGeodesics_main.m
@@ -11,3 +11,19 @@ Data = FlatDataExample;
 [hop,maxclass,minclass] = HOPClasses(hop,maxindex,minindex,DT);
 maxconnect = hopmaxconnect(DT.X,maxclass,hop);
 [GoodMaxGeodesics, maxconnect] = SelectGeodesics(maxconnect,maxclass);
+
+fprintf('\nDo you want to plot the results? \n');
+while true
+   fprintf('   1) Yes.\n')
+   fprintf('   2) No.\n')
+   plot_option = input('Choose one of the above: ');
+   if plot_option == 2
+      clear plot_option
+      break
+   elseif plot_option == 1
+      clear plot_option
+      run('GeodesicPlot')
+   else
+      fprintf('ERROR! You must enter 1 or 2.\n\n')
+   end
+end
