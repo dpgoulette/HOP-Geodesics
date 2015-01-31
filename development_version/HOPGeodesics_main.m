1c256971ca5653379da607866a126158e9fe57fe
David Goulette <dpgoulette@gmail.com>
2015-01-30 18:48:09 -0800

    Added ability to rerun the model.
    HOPGeodesics_main saves the key structures that are calculated in a
    struct called last_run_data.  Now if you rerun the model, it will pull
    DT, VV, VC etc from this struct and it doesn't have to recalculate.

--- development_version/HOPGeodesics_main.m
+++ development_version/HOPGeodesics_main.m
@@ -3,9 +3,11 @@
 
 % Issues:
 %
+%     - AlphaOneCells3d and AlphaOneCells2d need comments and cleanup.
+%
 %     - restructure the main and dependencies for clarity.  Push the user
 %     questions into the functions where possible.  Have the main only save
-%     the main data needed for data analysis. 
+%     the main data needed for data analysis.
 %
 %     - hopmaxconnect still uses
 %     maxconnect internally.  It works but the code is not as simple as it
@@ -46,14 +48,24 @@
 % Data = load('3D_example.txt');
 Data = load('FlatDataExample.txt');
 
-% Prepare the raw data for HOP. Calculate the Delaunay triangulation and the
-% Voroinoi diagram.  Then throw away "bad" data on the boundary of the data
-% space.  Also, the user has an option to calculate an alpha complex in
-% various ways.
-[DT, VV, VC, GoodEdges, GoodIndex] = HOPDataPrepare(Data);
+% Prepare the raw data for HOP. Calculate the Delaunay triangulation and
+% the Voroinoi diagram.  Then throw away "bad" data on the boundary of the
+% data space.  Also, the user has an option to calculate an alpha complex
+% in various ways. 
+%
+% NOTE! The three possibilities for this function are to allow for the user
+% to rerun the model on a large data set without having to recalculate DT,
+% VV and VC.  See the help section of this file and HOPDataPrepare.
+
+if exist('last_run_data','var') == 1
+   [DT, VV, VC, GoodEdges, GoodIndex, edge_alpha] =...
+      HOPDataPrepare(Data, last_run_data);
+else
+   [DT, VV, VC, GoodEdges, GoodIndex, edge_alpha] = HOPDataPrepare(Data);
+end
 
 
-%%%%%%%%%%%%%% HOP and related data structures %%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%% HOP and related data structures %%%%%%%%%%%%%%%%%
 % The next two functions collectively do the HOP algorithm and catalog a
 % variety of related data that result from doing HOP.  The key results are
 % saved in the hop, maxclass and minclass structs.
@@ -64,22 +76,23 @@ Data = load('FlatDataExample.txt');
 % Now create the maxclass and minclass structure which holds all of the
 % information for each hop max/min class.
 [hop, maxclass, minclass] = HOPClasses(hop,maxindex,minindex,DT);
-%
-%%%%%% Finished with the HOP section and struct creation.
+
+%%%%%% Finished with the HOP section and struct creation. %%%%%%
+
 
 
 %%%%%%%%%%%%%%% HOP Geodesics section %%%%%%%%%%%%%%%
 % In this section we find the geodesic paths which connect neighboring
 % maxima.  The results in this section are all stored in the maxclass
 % struct.
-%
+
 % Calculate all of the geodesics connecting neighboring max classes.
 maxclass = hopmaxconnect(DT.X,maxclass,hop);
-%
+
 % Select the geodesics that we will keep and which we will throw out (if
 % any).
 maxclass = SelectGeodesics(maxclass, hop);
-%
+
 % The geodesics are already the 1D connections between maxima.  Now we want
 % to find where geodesics form triangles (2D structure) and tetras (3D
 % structure).  Of course there will only be tetras if the data is 3D to
@@ -88,15 +101,20 @@ maxclass = GeoTris(maxclass);
 if size(DT.X,2) == 3
    maxclass = GeoTetras(maxclass);
 end
-%
+
 % The following is a temporary function.  It classifies each geodesic as
 % either being in a tetra, triangle, or only an isolated geodesic.  This
 % roughly means that the geodesic is a part of a 3D, 2D or 1D structure.
 maxclass = geodesic_classify(maxclass);
-%
-% Finished finding hop geodesics and related structures
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Finished finding hop geodesics and related structures
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% save the main data structures that were calculated in a struct to speed
+% up rerunning the model.
+last_run_data = struct('DT', {DT}, 'VV', {VV}, 'VC', {VC},...
+   'GoodIndex',{GoodIndex}, 'GoodEdges',{GoodEdges},...
+   'edge_alpha',{edge_alpha});
 
 % Plotting section
 fprintf('\nDo you want to plot the results? \n');
