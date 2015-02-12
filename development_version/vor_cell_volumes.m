967fdd84a438f950f7b369f89a1f0c1fa2233616
David Goulette <dpgoulette@gmail.com>
2015-02-12 11:02:20 -0800

    Improved vor_cell_volumes
    Improved small inefficiency with indexing.

--- development_version/vor_cell_volumes.m
+++ development_version/vor_cell_volumes.m
@@ -1,4 +1,4 @@
-function  Vols=vor_cell_volumes(V,C)
+function  Vols=vor_cell_volumes(VV,VC,GoodIndex)
 % volumes - finds the volumes of the delaunay tetrahedra. Note that an
 %       important part of this script relies on the fact that the point at
 %       infinity is stored in the first entry in the cell.  
@@ -6,13 +6,14 @@ function  Vols=vor_cell_volumes(V,C)
 %       V is the matrix of voronoi vertices
 %       C is the cell containing the indexes of each V-cel
 
-Vols=zeros(length(C),1);
+% create a volume list as long as the number of data points.  Bad data
+% points will ultimately have a 0 for its density.
+Vols=zeros(length(VC),1);
 
-for a=1:length(C)
-    if C{a}~=1 %check that the cell does NOT contain point at infinity
-        VcellVerts=V(C{a},:);%use the C{a} index vector to get V-cell verts
-        [~, Vols(a)]=convhull(VcellVerts); %volume of V-cell
-    end
+for a=1:length(GoodIndex)
+   % get the V-cell verts
+   VcellVerts = VV( VC{GoodIndex(a)}, :);
+   [~, Vols(GoodIndex(a))]=convhull(VcellVerts); %volume of V-cell
     
     % Update progress to the terminal.
     if mod(a,100)==0 && mod(a,1000) ~= 0
